fe7a776256efc34383edac550ba8184a537e1615
Garth天卜 <g-art-h@outlook.com>
2025-04-11 00:53:28 +0800

    完成

--- src/image_stacker.rs
+++ src/image_stacker.rs
@@ -7,7 +7,7 @@ pub(crate) fn stack_and_save(
     result_path: PathBuf,
     mode: u8,
 ) -> Result<(), &'static str> {
-    let mut buffer: Vec<f64> = Vec::new();
+    let mut buffer: Vec<f32> = Vec::with_capacity(1048576);
     let mut dimensions = None; // throw an error when dimensions mismatch
 
     for (index, path) in image_paths.iter().enumerate() {
@@ -22,20 +22,22 @@ pub(crate) fn stack_and_save(
                 return Err("Dimensions of images do not match");
             }
         } else {
-            buffer = pixels.iter().map(|&v| v as f64).collect();
+            print!("Stacking image 1/{} ...", image_paths.len());
+            buffer = pixels.par_iter().map(|&v| v as f32).collect();
             dimensions = Some((width, height));
             continue;
         }
 
         // Stack the image
+        print!("\rStacking image {}/{} ...", index + 1, image_paths.len());
         buffer
             .par_iter_mut()
             .zip(pixels.par_iter())
             .for_each(|(old_val, &new_val)| {
                 *old_val = update_pixel(*old_val, new_val, mode, index);
             });
-        println!("\rStacked image {}/{}", index + 1, image_paths.len());
     }
+    println!();
 
     let result_pixels: Vec<u16> = buffer.par_iter().map(|v| v.round() as u16).collect();
     let (width, height) = dimensions.ok_or("No images found")?;
@@ -49,11 +51,11 @@ pub(crate) fn stack_and_save(
     Ok(())
 }
 
-fn update_pixel(old_val: f64, new_val: u16, mode: u8, index: usize) -> f64 {
+fn update_pixel(old_val: f32, new_val: u16, mode: u8, index: usize) -> f32 {
     match mode {
-        0 => old_val * ((index - 1) as f64 / index as f64) + new_val as f64 / index as f64,
-        1 => old_val.max(new_val as f64),
-        2 => old_val.min(new_val as f64),
+        0 => old_val * ((index - 1) as f32 / index as f32) + new_val as f32 / index as f32,
+        1 => old_val.max(new_val as f32),
+        2 => old_val.min(new_val as f32),
         _ => panic!("Invalid mode"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,23 @@ mod console_reader;
 mod image_stacker;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Fast Image Stacker!");
+    println!("Version 0.1.0 (2025-04-11)");
+    println!("Author: GarthTB <g-art-h@outlook.com>");
+    println!("Repo: https://github.com/GarthTB/fast_image_stacker");
+
+    fn exit_with_error<T>(message: &str) -> T {
+        println!("Error: {message}\nExiting...");
+        console_reader::read_line();
+        std::process::exit(1);
+    }
+
+    let (image_paths, result_path) =
+        console_reader::get_paths().unwrap_or_else(|e| exit_with_error(e));
+    let mode = console_reader::get_mode();
+    image_stacker::stack_and_save(image_paths, result_path, mode)
+        .unwrap_or_else(|e| exit_with_error(e));
+
+    println!("Done!");
+    console_reader::read_line();
 }
